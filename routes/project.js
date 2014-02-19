6909e437e5d424919027d6586ce5aaac9d9557a4
vagrant <vagrant@precise32.(none)>
2014-02-19 23:11:54 +0000

    database mongo lab

--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,7 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
+  models.Project.find({"_id": projectID}).exec(afterQuery);
   // query for the specific project and
   // call the following callback
 
@@ -16,13 +16,27 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
-  // make a new Project and save it to the DB
-  // YOU MUST send an OK response w/ res.send();
+  var newPost = new models.Project({
+      "title": form_data.project_title,
+      "date": form_data.date,
+      "summary": form_data.summary,
+      "image": form_data.image_url
+  })
+
+  newPost.save(afterSaving);
+
+  function afterSaving(err, projects){
+      if(err){console.log(err); res.send(500);}
+      res.redirect('/');
+  }
 }
+
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+  models.Project.find({"_id": projectID}).remove().exec(afterDelete);
 
-  // find the project and remove it
-  // YOU MUST send an OK response w/ res.send();
+  function afterDelete(err, projects){
+    res.send()
+  }
 }
